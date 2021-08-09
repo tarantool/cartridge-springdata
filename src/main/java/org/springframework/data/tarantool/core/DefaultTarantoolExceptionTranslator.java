1861d148bb00460ee851f6086e0bb35923d1e920
Vladimir Rogach <v.rogach@corp.mail.ru>
2021-08-09 16:47:29 +0300

    Bump driver version to 0.5.0

--- src/main/java/org/springframework/data/tarantool/core/DefaultTarantoolExceptionTranslator.java
+++ src/main/java/org/springframework/data/tarantool/core/DefaultTarantoolExceptionTranslator.java
@@ -1,12 +1,9 @@
 package org.springframework.data.tarantool.core;
 
 import io.tarantool.driver.exceptions.TarantoolClientException;
-import io.tarantool.driver.exceptions.TarantoolFunctionCallException;
-import io.tarantool.driver.exceptions.TarantoolIndexNotFoundException;
 import io.tarantool.driver.exceptions.TarantoolException;
-import io.tarantool.driver.exceptions.TarantoolServerException;
-import io.tarantool.driver.exceptions.TarantoolSpaceFieldNotFoundException;
-import io.tarantool.driver.exceptions.TarantoolSpaceNotFoundException;
+import io.tarantool.driver.exceptions.TarantoolFunctionCallException;
+import io.tarantool.driver.exceptions.TarantoolInternalException;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.dao.InvalidDataAccessResourceUsageException;
@@ -23,11 +20,11 @@ public class DefaultTarantoolExceptionTranslator implements TarantoolExceptionTr
     public DataAccessException translateExceptionIfPossible(RuntimeException cause) {
         if (cause instanceof TarantoolException) {
             // TODO Superclass in driver for metadata exceptions
-            if (cause instanceof TarantoolClientException) {
+            if (cause instanceof TarantoolClientException|| cause instanceof TarantoolFunctionCallException) {
                 return new InvalidDataAccessResourceUsageException(cause.getMessage(), cause);
             }
-            if (cause instanceof TarantoolServerException || cause instanceof TarantoolFunctionCallException) {
-                return new DataRetrievalFailureException(cause.getMessage(),  cause);
+            if (cause instanceof TarantoolInternalException) {
+                return new DataRetrievalFailureException(cause.getMessage(), cause);
             }
             return new RecoverableDataAccessException(cause.getMessage(), cause);
         }
