19891d8fe55f7fbde035541e77bcacc7f6ff0578
Oleg Kuznetsov <oleg.kuznetsov@corp.mail.ru>
2021-12-23 18:40:00 +0300

    Fixes flacky tests

--- src/test/resources/cartridge/topology.lua
+++ src/test/resources/cartridge/topology.lua
@@ -1,15 +1,13 @@
 cartridge = require('cartridge')
-replicasets = {{
-    alias = 'app-router',
-    roles = {'app.roles.api_router'},
-    join_servers = {{uri = 'localhost:3301'}}
-}, {
-    alias = 's1-storage',
-    roles = {'app.roles.api_storage'},
-    join_servers = {{uri = 'localhost:3302'}, {uri = 'localhost:3303'}}
-}, {
-    alias = 's2-storage',
-    roles = {'app.roles.api_storage'},
-    join_servers = {{uri = 'localhost:3304'}, {uri = 'localhost:3305'}}
-}}
-return cartridge.admin_edit_topology({replicasets = replicasets})
+
+replicasets = { {
+                    alias = 'app-router',
+                    roles = { 'app.roles.api_router' },
+                    join_servers = { { uri = 'localhost:3301' } }
+                }, {
+                    alias = 's1-storage',
+                    roles = { 'app.roles.api_storage' },
+                    join_servers = { { uri = 'localhost:3302' } }
+                } }
+
+return cartridge.admin_edit_topology({ replicasets = replicasets })
