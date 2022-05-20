8d10bd852404ad3024c0215bd73027b8f7dc8bd2
Artyom Dubinin <rusartdub@gmail.com>
2022-05-20 18:54:55 +0300

    Fix checkstyle warnings

--- src/main/java/org/springframework/data/tarantool/repository/TarantoolSerializationType.java
+++ src/main/java/org/springframework/data/tarantool/repository/TarantoolSerializationType.java
@@ -7,33 +7,25 @@ import org.springframework.data.tarantool.core.mapping.Tuple;
  * This can be used both for validation and to speed up the conversion.
  * <ul>
  *   <li>
- *     <p>
  *       <b>TUPLE</b> - can be used to accept flatten structures from Tarantool that don't have keys to map the incoming object
  *       to a java class object. For mapping, the keys(metadata) are obtained by calling the lua function <code>ddl.get_schema</code>.
  *       Accordingly, the returnType class must have a camelCase name corresponding to snake_cased space name in Tarantool,
  *       or this snake_cased name must be specified in the {@link Tuple} annotation.
  *       Also, the field structure should be the same as in the corresponding Tarantool space format.
- *       <p>
- *         These flat structures can be passed to a connector with these serialization types:
- *         <ul>
- *           <li><a href="https://www.tarantool.io/en/doc/latest/reference/reference_lua/box_space/">box.space</a> function response</li>
- *           <li><a href="https://github.com/tarantool/crud">crud</a> function response</li>
- *           <li><i>MP_ARRAY</i> - an array suitable for the metadata structure(in lua it's number indexed table)</li>
- *         </ul>
- *       </p>
- *     </p>
+ *       These flat structures can be passed to a connector with these serialization types:
+ *       <ul>
+ *         <li><a href="https://www.tarantool.io/en/doc/latest/reference/reference_lua/box_space/">box.space</a> function response</li>
+ *         <li><a href="https://github.com/tarantool/crud">crud</a> function response</li>
+ *         <li><i>MP_ARRAY</i> - an array suitable for the metadata structure(in lua it's number indexed table)</li>
+ *       </ul>
  *   </li>
  *   <li>
- *     <p>
  *       <b>AUTO</b> - accepts all structures that TUPLE accepts.
- *       <p>
- *         Additionally, it can also accept:
- *         <ul>
- *           <li><i>MP_MAP</i> - map with keys equal to the returnType object fields(in lua it's key indexed table)</li>
- *           <li>Scalar message pack types (in lua it's number, string, boolean, etc.)</li>
- *         </ul>
- *       </p>
- *     </p>
+ *       Additionally, it can also accept:
+ *       <ul>
+ *         <li><i>MP_MAP</i> - map with keys equal to the returnType object fields(in lua it's key indexed table)</li>
+ *         <li>Scalar message pack types (in lua it's number, string, boolean, etc.)</li>
+ *       </ul>
  *   </li>
  * </ul>
  *
